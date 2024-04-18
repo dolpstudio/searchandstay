8e88bcd9893cf839bf7a19fb7496bbe7189494f1
Ramon <ramon.nogueira@hotmail.com>
2024-04-18 16:19:21 -0300

    add book controller, request, resource and collection

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Models\Book;
+use App\Http\Requests\BookRequest;
+use App\Http\Resources\BookCollection;
+use App\Http\Resources\BookResource;
+
+
+class BookController extends Controller
+{
+    /**
+     * Create a new Auth controller instance.
+     * Add middlewares
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): JsonResponse
+    {
+
+        return response()->json(
+            new BookCollection($request->user()->books),
+        );
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(BookRequest $request): JsonResponse
+    {
+
+        $book = $request->user()->books()->create($request->toArray());
+
+        return response()->json([
+            'message' => 'Book created successfully',
+            'expense' => new BookResource($book),
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book, Request $request): JsonResponse
+    {
+        return response()->json([
+            'expense' => new BookResource($book),
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(BookRequest $request, Book $book): JsonResponse
+    {
+
+        $book->fill($request->all());
+        $book->save();
+
+        return response()->json([
+            'message' => 'Book updated successfully',
+            'expense' => new BookResource($book),
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book, Request $request): JsonResponse
+    {
+        $book->delete();
+
+        return response()->json([
+            'message' => 'Book deleted successfully',
+        ], 200);
+    }
+}
---
+++ app/Http/Requests/BookRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is logged in.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:191',
+            'isbn' => 'required|integer|size:13',
+            'bookvalue' => 'required|regex:/^\d+(\.\d{1,2})?$/|min:0',
+        ];
+    }
+}
+
+
+
